e624a5a435688fa57d8c633fc7b0d9da120c1722
seenamakari <seenamakari@gmail.com>
2023-10-02 19:07:19 -0400

    Update Assignment_1.py

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -6,8 +6,15 @@ df = pd.read_csv('WE.csv')
 # Set the date column as the index
 df.set_index('Date', inplace=True)
 
+
+
+#Adding the column for simple daily returns
+df['simple_daily_return'] = (df['Close'] / df['Open'] - 1)
+
 # Retrieve data for specific dates
 specific_date_data = df.loc['2022-06-23']
+
+
 
 # Analyze or use the retrieved data
 print(specific_date_data)
