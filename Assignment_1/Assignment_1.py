391933ed35d93ff3ea74bd0eecee9de162393bbf
seenamakari <seenamakari@gmail.com>
2023-10-02 18:06:08 -0400

    Restructuring the repo

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
 # Read your dataset into a DataFrame
-df = pd.read_csv('your_dataset.csv')
+df = pd.read_csv('WE.csv')
 
 # Set the date column as the index
-df.set_index('date_column_name', inplace=True)
+df.set_index('Date', inplace=True)
 
 # Retrieve data for specific dates
-specific_date_data = df.loc['2023-10-01']
-date_range_data = df.loc['2023-10-01':'2023-10-10']
-specific_dates_data = df.loc[['2023-10-01', '2023-10-05', '2023-10-10']]
+specific_date_data = df.loc['2022-06-23']
 
 # Analyze or use the retrieved data
 print(specific_date_data)
-print(date_range_data)
-print(specific_dates_data)
+
