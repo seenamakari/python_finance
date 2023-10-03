eba2ad1b5c8011f4836798c914d8a3331d9f8308
seenamakari <seenamakari@gmail.com>
2023-10-02 20:06:50 -0400

    Update 1.1.py

--- Assignment_1/1.1.py
+++ Assignment_1/1.1.py
@@ -9,17 +9,23 @@ df.set_index('Date', inplace=True)
 #Adding the column for simple daily returns
 df['simple_daily_return'] = (df['Close'] / df['Open'] - 1)
 
+#Adding the column for gross daily returns
+df['gross_daily_return'] = (df['Close'] / df['Open'] + 1)
+
 # Answer for a
-simple_return_2022_06_23 = df.loc['2022-06-23']
-print(simple_return_2022_06_23)
+data_2022_06_23 = df.loc['2022-06-23']
+print(data_2022_06_23)
 
 #Answer for b
-simple_return_2022_07_01 = df.loc['2022-07-01']
-print(simple_return_2022_07_01)
+data_2022_07_01 = df.loc['2022-07-01']
+print(data_2022_07_01)
 
 #Answer for c
-simple_return_2023_03_28 = df.loc['2023-03-28']
-print(simple_return_2023_03_28)
+data_2023_03_28 = df.loc['2023-03-28']
+print(data_2023_03_28)
+
+#Answer for d
+
 
 
 
