ff178dc50c545a5e720cf173e286416a25d0b172
seenamakari <seenamakari@gmail.com>
2023-10-05 16:55:12 -0400

    Adding assignment files

--- Assignment_1/1.1.py
+++ Assignment_1/1.1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# Read your dataset into a DataFrame
+# Change dataset into a DataFrame
 df = pd.read_csv('WE.csv')
 
 # Set the date column as the index
@@ -12,19 +12,29 @@ df['simple_daily_return'] = (df['Close'] / df['Open'] - 1)
 #Adding the column for gross daily returns
 df['gross_daily_return'] = (df['Close'] / df['Open'] + 1)
 
-# Answer for a
+# 1.1.1.a What is the simple return on Jun 23, 2022?
 data_2022_06_23 = df.loc['2022-06-23']
 print(data_2022_06_23)
 
-#Answer for b
+#1.1.1.b What the simple return on Jul 01, 2022?
 data_2022_07_01 = df.loc['2022-07-01']
 print(data_2022_07_01)
 
-#Answer for c
+#1.1.1.c What about Mar 28, 2023?
 data_2023_03_28 = df.loc['2023-03-28']
 print(data_2023_03_28)
 
-#Answer for d
+#1.1.1.d What is the gross return on Jun 23, 2022?
+data_2022_06_23 = df.loc['2022-06-23']
+print(data_2022_06_23)
+
+#1.1.1.e What is the gross return on Jul 01, 2022?
+data_2022_07_01 = df.loc['2022-07-01']
+print(data_2022_07_01)
+
+
+
+
 
 
 
